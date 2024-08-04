ece2b91aa97b18975249d34e1ed9b0b3a47f3281
shashi <shashi@gmaill.com>
2024-08-04 15:50:54 +0530

    basic set done.

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='App'>
+      <h3>This is About Page</h3>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='App'>
+      <h1> This is iNotebook</h1>
+    </div>
+  )
+}
+
