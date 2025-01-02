8f55cc0ec96354fe2c080bfea73d9a4d3b3288de
shashikant021 <shashikantsahu487@gmail.com>
2025-01-02 19:01:53 +0530

    warning issue solved

--- Backend/db.js
+++ Backend/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 const mongoURI = process.env.REACT_APP_DATABASE_URL;
 
+mongoose.set('strictQuery', false);
+
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully !");
@@ -9,3 +11,5 @@ const connectToMongo = () => {
 }
 
 module.exports = connectToMongo;
+
+
