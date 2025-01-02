f4c8eb32d1a5e7aea5880e8ecc5000387a81b82b
shashikant021 <shashikantsahu487@gmail.com>
2025-01-02 18:42:51 +0530

    create iNotebook app with the help of MERN Stack

---
+++ Frontend/src/components/Login.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import { useNavigate  } from 'react-router-dom'
+  const host = "http://localhost:5000";
+//   const host = "https://inotebook-backend-m8bt.onrender.com";
+
+
+const Login = (props) => {
+    const [credentials, setCredentials] = useState({email: "", password: ""}) 
+    let navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(`${host}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: credentials.email, password: credentials.password})
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success){
+            // Save the auth token and redirect
+            localStorage.setItem('token', json.authtoken); 
+            // history.push("/");
+            props.showAlert("Logged in  Successfully", "Success");
+            navigate('/');
+
+        }
+        else{
+            props.showAlert("Invalid Details", "danger");
+        }
+    }
+
+    const onChange = (e)=>{
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
+
+    return (
+        <div className='mt-5 log_in'>
+            <h3> Login to Continue to iNotebook</h3>
+            <form  onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label fs-5">Email address</label>
+                    <input type="email" className="form-control" width="500px" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label fs-5">Password</label>
+                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} name="password" id="password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
---
+++ Frontend/src/components/Signup.js
@@ -0,0 +1,68 @@
+import React, {useState}  from 'react'
+import { useNavigate  } from 'react-router-dom'
+  const host = "http://localhost:5000";
+  // const host = "https://inotebook-backend-m8bt.onrender.com";
+
+const Signup = (props) => {
+
+  const [credentials, setCredentials] = useState({name:"", email: "", password: "", cpassword:"" }) 
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      const {name, email, password} = credentials;
+      const response = await fetch(`${host}/api/auth/createuser`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({name, email, password})
+      });
+      const json = await response.json()
+      console.log(json);
+      if (json.success){
+          // Save the auth token and redirect
+          localStorage.setItem('token', json.authtoken); 
+          // history.push("/");
+          navigate('/');
+          props.showAlert("Account Created Successfully", "Success");
+
+      }
+      else{
+          props.showAlert("Invalid credentials", "danger");
+      }
+  }
+
+  const onChange = (e)=>{
+      setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+
+  return (
+    <div className='container log_in'>
+       <h3> Create an account to Use iNotebook</h3>
+      <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+          <label htmlFor="name" className="form-label fs-5">Name</label>
+          <input type="name" className="form-control" id="name" name='name' onChange={onChange} aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label fs-5">Email address</label>
+          <input type="email" className="form-control" id="email" name='email' onChange={onChange} aria-describedby="emailHelp" />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Password" className="form-label fs-5">Password</label>
+          <input type="password" className="form-control" id="Password" name='password' onChange={onChange} minLength={5} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cPassword" className="form-label fs-5">Confirm Password</label>
+          <input type="password" className="form-control" id="cPassword" name='cpassword' onChange={onChange} minLength={5} required/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default Signup
